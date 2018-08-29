c29ba77a90d48f2bfc8db6aa9ccf95fb0ae559c5
宁宁 <383514580@qq.com>
2018-08-29 16:31:15 +0800

    重新定义手势的事件数据类型

--- src/interface.d.ts
+++ src/interface.d.ts
@@ -1,4 +1,5 @@
-export interface AnyPointers {
+// AnyTouch手势的事件数据
+export interface AnyTouchEvent {
     isStart: boolean,
     isMove: boolean,
     isEnd: boolean,
@@ -14,35 +15,22 @@ export interface AnyPointers {
     length?: number
 }
 
-export interface Session extends AnyPointers {}
+export interface Session extends AnyTouchEvent { }
 
-
-export interface handlerBus{
-    rotate?: [()=>{}];
-    pinch?: [()=>{}];
-    tap?: [()=>{}];
-    doubletap?: [()=>{}];
-    press?: [()=>{}];
-    pan?: [()=>{}];
-    swipe?: [()=>{}];
-    touchStart?: [()=>{}];
-    touchMove?: [()=>{}];
-    touchEnd?: [()=>{}];
+// 手势的触发函数
+export interface HandlerBus {
+    rotate?: [(event: AnyTouchEvent) => void];
+    pinch?: [(event: AnyTouchEvent) => void];
+    tap?: [(event: AnyTouchEvent) => void];
+    doubletap?: [(event: AnyTouchEvent) => void];
+    press?: [(event: AnyTouchEvent) => void];
+    pan?: [(event: AnyTouchEvent) => void];
+    swipe?: [(event: AnyTouchEvent) => void];
+    touchStart?: [(event: AnyTouchEvent) => void];
+    touchMove?: [(event: AnyTouchEvent) => void];
+    touchEnd?: [(event: AnyTouchEvent) => void];
 }
 
-export interface EventHandler {
-    rotate?: EventListener;
-    pinch?: EventListener;
-    tap?: EventListener;
-    doubletap?: EventListener;
-    press?: EventListener;
-    pan?: EventListener;
-    swipe?: EventListener;
-    touchStart?: EventListener;
-    touchMove?: EventListener;
-    touchEnd?: EventListener;
-    [propName: string]:any;
-}
 
 export interface Recognizer {
     type: String;
@@ -52,28 +40,8 @@ export interface Recognizer {
     end?: Function;
 }
 
+// 向量
 export interface Vector {
     x: number;
     y: number;
-}
-
-export interface EventHandler {
-    rotate?: EventListener;
-    pinch?: EventListener;
-    tap?: EventListener;
-    doubletap?: EventListener;
-    press?: EventListener;
-    pan?: EventListener;
-    swipe?: EventListener;
-    touchStart?: EventListener;
-    touchMove?: EventListener;
-    touchEnd?: EventListener;
-}
-
-export interface Recognizer {
-    type: String;
-    computedData: Function;
-    start?: Function;
-    move?: Function;
-    end?: Function;
 }
