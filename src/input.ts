77452756e75bc172564f7577ddae4f8cfc53ebda
宁宁 <383514580@qq.com>
2018-09-02 21:10:27 +0800

    把格式化的input放在session.input上

--- src/input.ts
+++ src/input.ts
@@ -1,30 +1,28 @@
 import session from './session';
-export default function(event: TouchEvent) {
-        const pointers = event.touches;
-        const changedPointers = event.changedTouches;
-        const { now } = Date;
-
-        session.length = pointers.length;
-
-        session.isStart = session.isEnd;
-        session.isEnd = 0 === pointers.length;
-        session.isMove = !session.isStart && !session.isEnd;
-        // 上一步的触点
-        session.prevPointers = session.activePointers;
-        // 起始点
-        if (session.isStart) {
-            session.startPointers = pointers;
-            session.startTime = now();
-        }
-        // 当前点
-        session.activePointers = pointers;
-        session.activeTime = now();
-        // 结束点
-        if (session.isEnd) {
-            session.activePointers = changedPointers;
-            session.endPointers = changedPointers;
-            session.endTime = now();
-        }
-
-        return session;
+import compute from './compute';
+export default function (event: TouchEvent) {
+    const pointers = event.touches;
+    const changedPointers = event.changedTouches;
+    const { now } = Date;
+    session.input.length = pointers.length;
+    session.input.isStart = session.input.isEnd;
+    session.input.isEnd = 0 === pointers.length;
+    session.input.isMove = !session.input.isStart && !session.input.isEnd;
+    // 上一步的触点
+    session.input.prevPointers = session.input.activePointers;
+    // 起始点
+    if (session.input.isStart) {
+        session.input.startPointers = pointers;
+        session.input.startTime = now();
+    }
+    // 当前点
+    session.input.activePointers = pointers;
+    session.input.activeTime = now();
+    // 结束点
+    if (session.input.isEnd) {
+        session.input.activePointers = changedPointers;
+        session.input.endPointers = changedPointers;
+        session.input.endTime = now();
+    }
+    return session.input;
 };
