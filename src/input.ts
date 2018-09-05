0cd96c81a98a7e0fc3cf7523aae18b69421fbe26
宁宁 <383514580@qq.com>
2018-09-05 09:55:11 +0800

    增加每300ms重新计算startPointers, startTime , 用来识别当用户按住屏幕一段时间才开始操作的手势

--- src/input.ts
+++ src/input.ts
@@ -2,11 +2,12 @@ import session from './session';
 import compute from './compute';
 export default function (event: TouchEvent) {
     const pointers = event.touches;
+    const length  = pointers.length;
     const changedPointers = event.changedTouches;
     const { now } = Date;
-    session.input.length = pointers.length;
+    session.input.length = length;
     session.input.isStart = session.input.isEnd;
-    session.input.isEnd = 0 === pointers.length;
+    session.input.isEnd = 0 === length;
     session.input.isMove = !session.input.isStart && !session.input.isEnd;
     // 上一步的触点
     session.input.prevPointers = session.input.activePointers;
@@ -15,9 +16,19 @@ export default function (event: TouchEvent) {
         session.input.startPointers = pointers;
         session.input.startTime = now();
     }
+
     // 当前点
     session.input.activePointers = pointers;
     session.input.activeTime = now();
+
+    // 每300ms重新计算startPointers, startTime
+    // 用来识别当用户按住屏幕一段时间才开始操作的手势
+    if(session.input.isMove && 300 < session.input.activeTime - session.input.startTime) {
+        session.input.startPointers = pointers;
+        session.input.startTime = now();
+    }
+
+
     // 结束点
     if (session.input.isEnd) {
         session.input.activePointers = changedPointers;
