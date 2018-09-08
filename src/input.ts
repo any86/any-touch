db242b0acf44621fe6c4136684728a5e424523fa
宁宁 <383514580@qq.com>
2018-09-08 11:50:28 +0800

    增加reStartPointers/reStartTime,  每300ms更新当前触碰信息

--- src/input.ts
+++ src/input.ts
@@ -1,19 +1,10 @@
 import session from './session';
-import compute from './compute';
-
 export default function (event: TouchEvent) {
     const pointers = event.touches;
     const length = pointers.length;
     const changedPointers = event.changedTouches;
     const { now } = Date;
-
-    /**
-     * 重置起点
-     */
-    const resetStart = () => {
-        session.input.startPointers = pointers;
-        session.input.startTime = now();
-    };
+    const { max } = Math;
 
     // 触碰点数
     session.input.length = length;
@@ -23,14 +14,14 @@ export default function (event: TouchEvent) {
     session.input.isEnd = 0 === length;
     session.input.isMove = !session.input.isStart && !session.input.isEnd;
 
-    // 第一下触碰, 不是touchstart, 是手势识别后的第一次触碰
-    // session.input.firstTouch = pointers;
-
     // 上一步的触点
     session.input.prevPointers = session.input.activePointers;
+
     // [Start]
     if (session.input.isStart) {
-        resetStart();
+        // 记录起点信息
+        session.input.startPointers = pointers;
+        session.input.startTime = now();
     }
     // [Move]
     else if (session.input.isMove) {
@@ -41,9 +32,11 @@ export default function (event: TouchEvent) {
 
         // 每300ms重新计算startPointers, startTime
         // 用来识别当用户按住屏幕一段时间才开始操作的手势
-        const isLongHold = 300 < session.input.activeTime - session.input.startTime;
+        const isLongHold = 300 < session.input.activeTime - session.input.reStartTime;
         if (isMultiToSingle || isLongHold) {
-            resetStart();
+            // 新起点
+            session.input.reStartPointers = pointers;
+            session.input.reStartTime = now();
         }
     }
     // [End]
@@ -54,7 +47,16 @@ export default function (event: TouchEvent) {
 
     // 当前点数据
     session.input.activePointers = pointers;
-    // session.input.activePointers = 0 === pointers.length ? changedPointers : pointers;
     session.input.activeTime = now();
+
+    // 可用点, 当end阶段为离开时候的点
+    session.input.validPointers = 0 < session.input.activePointers.length ? session.input.activePointers : session.input.endPointers;
+
+    // 出现过的最大触点数量
+    if (undefined === session.input.maxLength) {
+        session.input.maxLength = length;
+    } else {
+        session.input.maxLength = max(session.input.maxLength, length);
+    }
     return session.input;
-};
+}; 
