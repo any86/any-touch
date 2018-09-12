638d3ff05c67360ae14a94497c50b48c2cb4d133
宁宁 <383514580@qq.com>
2018-09-12 14:51:30 +0800

    优化计算最终输入逻辑

--- src/compute.ts
+++ src/compute.ts
@@ -1,72 +1,121 @@
-import { InputComputed, AnyInput } from './interface';
-import { getCenter, getAngle, getVLength } from './vector';
 
-export default function (input: any): InputComputed {
+import session from './session';
+import { InputComputed, AnyTouch } from './interface';
+import { getCenter, getAngle, getVLength, getDirection } from './vector';
+import { propX, propY } from './const';
+import computeDeltaXY from './computeDeltaXY'
+import computeLast from './computeLast'
+
+export default function ({
+    startInput,
+    prevInput,
+    input, validInput, startMultiInput
+}: any): any {
     const { abs, round, max } = Math;
-    
-    // 每次变化产生的偏移
-    let deltaX, deltaY, absDeltaX, absDeltaY;
+    const length = input.pointers.length;
 
-    // 起始到结束的偏移
-    let displacementX = round(input.activePointers[0].pageX - input.startPointers[0].pageX);
-    let displacementY = round(input.activePointers[0].pageY - input.startPointers[0].pageY);
-    let distanceX = abs(displacementX);
-    let distanceY = abs(displacementY);
-    let distance = getVLength({x: distanceX, y: distanceY});
 
-    // 每次变化的位移增量
-    if (undefined !== input.prevPointers) {
-        deltaX = round(input.activePointers[0].pageX - input.prevPointers[0].pageX);
-        deltaY = round(input.activePointers[0].pageY - input.prevPointers[0].pageY);
-        absDeltaX = abs(deltaX);
-        absDeltaY = abs(deltaY);
+    let computed: any = {
+        // 起始到结束的偏移
+        displacementX: 0,
+        displacementY: 0,
+        distanceX: 0,
+        distanceY: 0,
+        distance: 0,
+
+        // 位移变化量
+        deltaX: 0,
+        deltaY: 0,
+        absDeltaX: 0,
+        absDeltaY: 0,
+
+        //  速率
+        velocityX: 0,
+        velocityY: 0,
+        maxVelocity: 0,
+
+        // 时间
+        duration: 0,
+
+        // 旋转和缩放
+        angle: 0,
+        scale: 1,
+        centerX: undefined, centerY: undefined,
+        lastVelocityY: undefined, lastVelocityX: undefined
+    };
+
+
+
+    // ================== 单点 ==================
+    // 有效点, 包含位置信息
+    if (2 > length) {
+        // 单指滑动计算
+        computed.displacementX = round(validInput.pointers[0][propX] - startInput.pointers[0][propX]);
+        computed.displacementY = round(validInput.pointers[0][propY] - startInput.pointers[0][propY]);
+        computed.distanceX = abs(computed.displacementX);
+        computed.distanceY = abs(computed.displacementY);
+        computed.distance = round(getVLength({ x: computed.distanceX, y: computed.distanceY }));
+
+        // 已消耗时间
+        computed.duration = input.timestamp - startInput.timestamp;
+
+        // 计算每次移动时产生的位移增量
+        const deltaData = computeDeltaXY(validInput, prevInput);
+        computed.deltaX = deltaData.deltaX;
+        computed.deltaY = deltaData.deltaY;
+
+        const velocityData = computeLast(deltaData);
+        computed.lastVelocityX = velocityData.velocityX;
+        computed.lastVelocityY = velocityData.velocityY;
+
+        computed.absDeltaX = abs(computed.deltaX);
+        computed.absDeltaY = abs(computed.deltaY);
+
+        // 速率
+        computed.velocityX = abs(computed.distanceX / computed.duration);
+        computed.velocityY = abs(computed.distanceY / computed.duration);
+        computed.maxVelocity = max(computed.velocityX, computed.velocityY);
+
+        // 计算方向
+        const direction: string = getDirection(computed.deltaX, computed.deltaY);
     }
-
-    // 从start到end的时间
-    const duration = input.activeTime - input.startTime;
-
-    // 速率
-    const velocityX = abs(displacementX / duration);
-    const velocityY = abs(displacementY / duration);
-    const maxVelocity = max(velocityX, velocityY);
-    // 多点
-    let angle = 0;
-    let scale = 1;
-    if (1 < input.length) {
+    // ================== 多点 ==================
+    else if (1 < prevInput.pointers.length) {
         const v0 = {
-            x: input.prevPointers[1].pageX - input.prevPointers[0].pageX,
-            y: input.prevPointers[1].pageY - input.prevPointers[0].pageY
+            x: prevInput.pointers[1][propX] - prevInput.pointers[0][propX],
+            y: prevInput.pointers[1][propY] - prevInput.pointers[0][propY]
         };
 
         const v1 = {
-            x: input.activePointers[1].pageX - input.activePointers[0].pageX,
-            y: input.activePointers[1].pageY - input.activePointers[0].pageY
+            x: input.pointers[1][propX] - input.pointers[0][propX],
+            y: input.pointers[1][propY] - input.pointers[0][propY]
         };
 
-        angle = getAngle(v0, v1);
-        scale = getVLength(v1) / getVLength(v0);
-    }
+        computed.angle = getAngle(v1, v0);
+        computed.scale = getVLength(v1) / getVLength(v0);
+
+
+    };
 
     // 中心
-    const { x: centerX, y: centerY } = getCenter(input.activePointers);
+    // const { x, y } = getCenter(input);
+    // computed.centerX = x;
+    // computed.centerY = y;
+
+    // 出现过的最大触点数量
+    if (session.isStart) {
+        session.maxLength = 0;
+    }
+    if (0 === session.maxLength) {
+        session.maxLength = length;
+    } else {
+        session.maxLength = max(session.maxLength, length);
+    }
 
     return {
-        velocityX,
-        velocityY,
-        maxVelocity,
-        scale,
-        angle,
-        centerX,
-        centerY,
-        deltaX,
-        deltaY,
-        absDeltaX,
-        absDeltaY,
-        displacementX,
-        displacementY,
-        distanceX,
-        distanceY,
-        distance,
-        duration
+        ...input,
+        length,
+        maxLength: session.maxLength,
+        ...computed
     };
 };
