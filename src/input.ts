a8afdbd9d5a125790a023085de5db36c365c5c7b
宁宁 <383514580@qq.com>
2018-09-12 14:52:22 +0800

    优化输入结构

--- src/input.ts
+++ src/input.ts
@@ -10,71 +10,58 @@ export default function (event: TouchEvent): any {
     const length = pointers.length;
     const changedPointers = event.changedTouches;
     const { max } = Math;
-
     // 判断当前状态
     session.isStart = session.isEnd;
     session.isEnd = 0 === length;
     session.isMove = !session.isStart && !session.isEnd;
 
-    // 上一步的触点
-    session.prevInput = session.input;
+    
 
     // [Start]
     if (session.isStart) {
-        // 记录起点信息
+        // 清空缓存的多点起点数据
+        session.startMultiInput = undefined;
+        // 起点(单点|多点)
         session.startInput = new InputFactory(pointers);
-        // 清空上一点和最终点
+        // 上一步的触点
         session.prevInput = undefined;
+        // 当前触碰点
+        session.input = new InputFactory(pointers);
     }
     // [Move]
     else if (session.isMove) {
-        // 当从多点触碰变为单点
-        // 该位置的代码完全是为了当pinch/rotate后进行pan操作的时候,
-        // 让distance相关能够以变为单点后的坐标进行计算
-        // const isMultiToSingle = 1 < session.prevInput.length && 1 === session.length;
-
-        // 每300ms重新计算startInput, startTime
-        // 用来识别当用户按住屏幕一段时间才开始操作的手势
-        // const isLongHold = 300 < session.activeTime - session.reStartTime;
-        // if (isMultiToSingle) {
-        // 新起点
-        // session.reStartPointers = pointers;
-        // session.reStartTime = now();
-        // }
+        // 上一步的触点
+        session.prevInput = session.input;
+        // 当前触碰点
+        session.input = new InputFactory(pointers);
     }
     // [End]
     else if (session.isEnd) {
         session.endInput = new InputFactory(changedPointers);
-        // if(1 < session.lastInputArray.length){
-        //     session.penultInput = session.lastInputArray[0];
-        // } else {
-        //     session.penultInput = undefined;
-        // }
     }
 
-    // 当前触碰点
-    session.input = new InputFactory(pointers);
+    session.validInput = 0 === session.input.pointers.length ? session.prevInput : session.input;
 
-
-    // 出现过的最大触点数量
-    if (undefined === session.maxLength) {
-        session.maxLength = length;
-    } else {
-        session.maxLength = max(session.maxLength, length);
+    // 开始多点触碰
+    if (1 < length && undefined === session.startMultiInput) {
+        session.startMultiInput = new InputFactory(pointers);
+    }
+    
+    if (undefined !== session.prevInput && 1 < session.prevInput.pointers.length && 1 === session.input.pointers.length) {
+        session.startInput = new InputFactory(pointers);
     }
 
-    const { 
+    let {
         startInput,
         prevInput,
-        input,
-        maxLength } = session;
+        input, startMultiInput
+    } = session;
 
     const computed = compute({
         startInput,
         prevInput,
-        input,
-        maxLength
+        input, startMultiInput,
+        validInput: session.validInput
     });
-    // console.log(session);
     return { ...input, ...computed };
 }; 
