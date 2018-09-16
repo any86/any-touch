1ebf32ddba60ed6a3bb8caed5e3870c2a992259e
宁宁 <383514580@qq.com>
2018-09-16 21:26:06 +0800

    删除swiper中的session代码

--- src/recognitions/Swipe.ts
+++ src/recognitions/Swipe.ts
@@ -1,13 +1,12 @@
-import session from '../session';
 export default class SwipeRecognizer {
-    recognize(computedInput: any, callback: (paylod: any) => {}) {
+    recognize(computedInput: any, callback: (paylod: any) => {}): void {
         if (this.test(computedInput)) {
             callback({ type: 'swipe', ...computedInput });
         }
     };
-
-    test(computedInput: any) {
-        const {lastVelocityX, lastVelocityY,maxLength} = computedInput;
-        return 1 === maxLength && 'end' === session.inputStatus && 0.3 < Math.max(Math.abs(lastVelocityX), Math.abs(lastVelocityY));
+    
+    test(computedInput: any): boolean {
+        const { status, lastVelocityX, lastVelocityY, maxLength } = computedInput;
+        return 1 === maxLength && 'end' === status && 0.3 < Math.max(Math.abs(lastVelocityX), Math.abs(lastVelocityY));
     };
 };
