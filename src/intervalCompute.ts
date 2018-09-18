4804ed027de6580d412f29a4b7a5a077fb694957
宁宁 <383514580@qq.com>
2018-09-18 09:50:56 +0800

    删除无用

--- src/intervalCompute.ts
+++
@@ -1,9 +0,0 @@
-import { Input,Computed } from './interface';
-import { getCenter, getAngle, getVLength, getDirection } from './vector';
-import { propX, propY } from './const';
-let prevInput;
-export default function (input:Input): any {
-    if(undefined === prevInput || prevInput.time) {
-        prevInput = input;
-    }
-};
