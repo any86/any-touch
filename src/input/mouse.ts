d67c1d294779e8f59db5a93d861f42a478b5a562
宁宁 <383514580@qq.com>
2018-09-19 09:00:39 +0800

    增加返回status和source字段

--- src/input/mouse.ts
+++ src/input/mouse.ts
@@ -1,18 +1,11 @@
-import session from '../session';
 let prevPointers: any[] = undefined;
 let isPressed = false;
 export default function (event: any): any {
     const { clientX, clientY, type } = event;
     const changedPointers = prevPointers;
-    const Map: any = {
-        mousedown: 'start',
-        mousemove: 'move',
-        mouseup: 'end'
-    };
-    session.inputStatus = Map[type];
-
     let pointers = prevPointers = [{ clientX, clientY }];
     let length = 1;
+    
     if ('mousedown' === type) {
         isPressed = true;
     } else if ('mousemove' === type) {
@@ -27,7 +20,17 @@ export default function (event: any): any {
         isPressed = false;
     }
 
+    const MAP: {
+        [propName: string]: string;
+    } = {
+        mousedown: 'start',
+        mousemove: 'move',
+        mouseup: 'end'
+    };
+
     return {
+        source: 'mouse',
+        status: MAP[type],
         changedPointers,
         pointers,
         length
