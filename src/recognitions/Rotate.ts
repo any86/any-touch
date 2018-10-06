bb92fd1df0e3442126111321da226fc692b5c94f
PC-Ning <383514580@qq.com>
2018-10-06 18:16:47 +0800

    完成rotatestart | rotatemove | rotateend

--- src/recognitions/Rotate.ts
+++ src/recognitions/Rotate.ts
@@ -1,11 +1,21 @@
-export default class RotateRecognizer {
+import Base from './Base';
+import { Computed } from '../interface';
+
+export default class RotateRecognizer extends Base {
     recognize(computed: any, callback: (paylod: any) => {}) {
         if (this.test(computed)) {
             callback({ type: 'rotate', ...computed });
+
+            //rotatestart |rotatemove |rotateend
+            const type = this.recognizeType(computed.nativeEventType);
+            callback({ type: 'rotate' + type, ...computed });
+
         }
     };
 
-    test({length}:any) {
-        return 1 < length;
+    test({ length, nativeEventType }: Computed) {
+        // 如果触碰点要大于1
+        // 如果已经识别, 并且当前事件是离开阶段
+        return 1 < length || ('end' === nativeEventType && this.isRecognized);
     };
 };
