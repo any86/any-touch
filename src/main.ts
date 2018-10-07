2c7b96fe67b8cdba56556e6727d31bddaa38e044
宁宁 <383514580@qq.com>
2018-10-07 17:17:10 +0800

    默认阻止默认事件

--- src/main.ts
+++ src/main.ts
@@ -105,7 +105,7 @@ export default class AnyTouch {
     };
 
     handler(event: TouchEvent) {
-        // event.preventDefault();
+        event.preventDefault();
         // computed为包含了计算值的input
         const computed = compute(event);
         // console.log(computed);
