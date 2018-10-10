eda1c80ff521c31f2716d6408f79f0bf5a3579f1
PC-Ning <383514580@qq.com>
2018-10-10 23:41:52 +0800

    检查deltaX逻辑

--- src/compute/computeDeltaXY.ts
+++ src/compute/computeDeltaXY.ts
@@ -4,15 +4,15 @@ export default function ({
 }: any): { deltaX: number, deltaY: number } {
     let deltaX: number;
     let deltaY: number;
-    if (undefined === prevInput) {
-        deltaX = 0;
-        deltaY = 0;
-    } else if ('end' === input.nativeEventType) {
+    if ('end' === input.nativeEventType || 'start' === input.nativeEventType) {
         deltaX = 0;
         deltaY = 0;
     } else {
         deltaX = input.centerX - prevInput.centerX;
         deltaY = input.centerY - prevInput.centerY;
     }
+
+    console.log({ deltaX, deltaY, type:input.nativeEventType });
+
     return { deltaX, deltaY };
 };
