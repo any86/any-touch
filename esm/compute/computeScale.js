ef367398fc38824ffe596f2393e136464b0f893b
PC-Ning <383514580@qq.com>
2018-10-30 21:33:57 +0800

    删除esm文件

--- esm/compute/computeScale.js
+++
@@ -1,7 +0,0 @@
-import { getVLength } from '../vector';
-export default function ({ startV, prevV, activeV }) {
-    const deltaScale = getVLength(activeV) / getVLength(prevV);
-    const scale = getVLength(activeV) / getVLength(startV);
-    return { scale, deltaScale };
-}
-;
--- esm/compute/computeVector.js
+++
@@ -1,5 +0,0 @@
-import { propX, propY } from '../const';
-export default (input) => ({
-    x: input.pointers[1][propX] - input.pointers[0][propX],
-    y: input.pointers[1][propY] - input.pointers[0][propY]
-});
