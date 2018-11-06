bc5e02b611db657abbe677d59d8ffeb4d627dce0
宁宁 <383514580@qq.com>
2018-11-06 17:05:53 +0800

    修改swipe的测试脚本, 修复之前模拟swipe逻辑有可能不触发的bug

--- test/swipe.spec.ts
+++ test/swipe.spec.ts
@@ -14,6 +14,7 @@ test('事件swipe是否正确?', (done) => {
     // 模拟touch触碰
     dispatchTouchStart(el, [{ x: 30, y: 0 }]);
     dispatchTouchMove(el, [{ x: 30, y: 20 }]);
+    dispatchTouchMove(el, [{ x: 30, y: 60 }]);
     dispatchTouchMove(el, [{ x: 30, y: 120 }]);
     setTimeout(() => {
         dispatchTouchEnd(el);
