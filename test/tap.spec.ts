ac3a6ce46dc3b0da94d919cb39795e334d2585c2
PC-Ning <383514580@qq.com>
2018-11-10 22:31:32 +0800

    增加requireFailure的测试用例

--- test/tap.spec.ts
+++ test/tap.spec.ts
@@ -20,41 +20,29 @@ test('仅有tap识别, 事件是否触发', async (done) => {
 });
 
 
-// test('tap之间的requireFailure是否生效?', async (done) => {
-//     const tap2 = new AnyTouch.TapRecognizer({ name: 'doubletap', pointer: 1, taps: 2 })
-//     const tap3 = new AnyTouch.TapRecognizer({ name: 'threetap', pointer: 1, taps: 3 })
-//     const anyTouch = new AnyTouch(document.getElementById('box'));
-//     anyTouch.add(tap2);
-//     anyTouch.add(tap3);
-//     const tap1 = anyTouch.get('tap');
-//     tap1.requireFailure(tap2);
-//     tap1.requireFailure(tap3);
-//     tap2.requireFailure(tap3);
-//     const at = new AnyTouch(el);
+test('tap与doubletap之间的requireFailure是否生效?', async (done) => {
+    const tap2 = new AnyTouch.TapRecognizer({ name: 'doubletap', pointer: 1, taps: 2 })
+    const tap3 = new AnyTouch.TapRecognizer({ name: 'threetap', pointer: 1, taps: 3 })
+    const at = new AnyTouch(el);
+    at.add(tap2);
+    at.add(tap3);
+    const tap1 = at.get('tap');
+    tap1.requireFailure(tap2);
+    tap1.requireFailure(tap3);
+    tap2.requireFailure(tap3);
 
-//     at.on('doubletap', (e) => {
-//         expect(e.type).toBe('doubletap');
-//         done();
-//     });
+    at.on('doubletap', (e) => {
+        expect(e.type).toBe('doubletap');
+        done();
+    });
 
-//     let lastTime:number = Date.now();
-//     el.addEventListener('touchstart', e=>{
-//         console.log(e.type,  Date.now() - lastTime);
-//         lastTime = Date.now();
-//     })
+    const ts = new TouchSimulator();
+    // 模拟touch触碰
+    ts.dispatchTouchStart(el, [{ x: 0, y: 0 }]);
+    ts.dispatchTouchEnd(el);
 
-//     el.addEventListener('touchend', e=>{
-//         console.log(e.type,  Date.now() - lastTime);
-//         lastTime = Date.now();
-//     })
+    await sleep(10);
+    ts.dispatchTouchStart(el, [{ x: 0, y: 0 }]);
+    ts.dispatchTouchEnd(el);
 
-//     // 模拟touch触碰
-//     dispatchTouchStart(el, [{ x: 0, y: 0 }]);
-//     await sleep(50);
-//     dispatchTouchEnd(el);
-
-//     await sleep(10);
-//     dispatchTouchStart(el, [{ x: 0, y: 0 }]);
-//     await sleep(50);
-//     dispatchTouchEnd(el);
-// });
+});
