a8d6c01a8319b05028f00117942fc9d81a22de31
宁宁 <383514580@qq.com>
2018-11-17 11:44:30 +0800

    增加codecov到ci

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,6 @@ module.exports = {
         userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
     },
     collectCoverage: true,
+    coverageDirectory: "./coverage/",
     // verbose:false,
 };
