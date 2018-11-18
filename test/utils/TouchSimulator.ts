84e448f44f2b121be94e0d0008b29ef56f137c91
PC-Ning <383514580@qq.com>
2018-11-18 19:59:14 +0800

    增加mouse事件模拟, 但是并没有开始测试, 需要修改jest的头信息, 稍后像个办法解决可以一次测试可以测试2种不同的ua

--- test/utils/TouchSimulator.ts
+++ test/utils/TouchSimulator.ts
@@ -2,6 +2,9 @@ interface Pointer {
     x: number;
     y: number;
 };
+interface Options {
+    device: 'touch' | 'mouse'
+};
 const CLIENT_X = 'clientX';
 const CLIENT_Y = 'clientY';
 export default class TouchSimulator {
@@ -11,9 +14,11 @@ export default class TouchSimulator {
     }[];
 
     public el: Element | Document;
+    public device: 'touch' | 'mouse';
 
-    constructor(el: Element | Document) {
+    constructor(el: Element | Document, { device = 'touch' }: Options = <Options>{}) {
         this.el = el;
+        this.device = device;
     };
 
     /**
@@ -22,7 +27,8 @@ export default class TouchSimulator {
      * @param {Pointer[]} 触点
      */
     public dispatchTouchStart(pointers: Pointer[]) {
-        let event: any = new Event('touchstart', {});
+        let type = 'touch' === this.device ? 'touchstart' : 'mousedown';
+        let event: any = new Event(type, {});
         event.touches = pointers.map(({ x, y }) => ({ [CLIENT_X]: x, [CLIENT_Y]: y }));
         event.changedTouches = pointers.map(({ x, y }) => ({ [CLIENT_X]: x, [CLIENT_Y]: y }));
         this.prevTouches = event.touches;
@@ -35,7 +41,8 @@ export default class TouchSimulator {
      * @param {Pointer[]} 触点
      */
     public dispatchTouchMove(pointers: Pointer[]) {
-        let event: any = new Event('touchmove', {});
+        let type = 'touch' === this.device ? 'touchmove' : 'mousemove';
+        let event: any = new Event(type, {});
         // 对应点不同就放进changedTouches;
         event.changedTouches = pointers.filter(({ x, y }, index) => {
             return (this.prevTouches[index][CLIENT_X] !== x || this.prevTouches[index][CLIENT_Y] !== y);
@@ -52,7 +59,8 @@ export default class TouchSimulator {
      * @param {Number} 删除多少个点
      */
     public dispatchTouchEnd(pointerIndex?: number, pointerNumber?: number) {
-        let event: any = new Event('touchend', {});
+        let type = 'touch' === this.device ? 'touchend' : 'mouseup';
+        let event: any = new Event(type, {});
         event.changedTouches = this.prevTouches.splice(pointerIndex || 0, pointerNumber || this.prevTouches.length);
         // 当前的this.prevTouches已经是减去了变化点后的数组
         event.touches = this.prevTouches;
