e28fc416cfa31bae82e23a52229f1f8552643f39
PC-Ning <383514580@qq.com>
2018-11-21 22:20:58 +0800

    删除esm文件夹

--- esm/compute/computeVector.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var const_1 = require("../const");
-exports["default"] = (function (input) { return ({
-    x: input.pointers[1][const_1.propX] - input.pointers[0][const_1.propX],
-    y: input.pointers[1][const_1.propY] - input.pointers[0][const_1.propY]
-}); });
-//# sourceMappingURL=computeVector.js.map
