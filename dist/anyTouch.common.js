18cb8aa2d401fef30d1e6e62d2acbdd454012adf
PC-Ning <383514580@qq.com>
2018-12-01 22:52:44 +0800

    fix: 解决rollup不解析commonjs格式的包

--- dist/anyTouch.common.js
+++ dist/anyTouch.common.js
@@ -74,6 +74,124 @@ function __spread() {
     return ar;
 }
 
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+
+function __values$1(o) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator], i = 0;
+    if (m) return m.call(o);
+    return {
+        next: function () {
+            if (o && i >= o.length) o = void 0;
+            return { value: o && o[i++], done: !o };
+        }
+    };
+}
+
+function __read$1(o, n) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator];
+    if (!m) return o;
+    var i = m.call(o), r, ar = [], e;
+    try {
+        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+    }
+    catch (error) { e = { error: error }; }
+    finally {
+        try {
+            if (r && !r.done && (m = i["return"])) m.call(i);
+        }
+        finally { if (e) throw e.error; }
+    }
+    return ar;
+}
+
+function __spread$1() {
+    for (var ar = [], i = 0; i < arguments.length; i++)
+        ar = ar.concat(__read$1(arguments[i]));
+    return ar;
+}
+
+var EventEmitter = (function () {
+    function EventEmitter() {
+        this.listenersMap = {};
+    }
+    EventEmitter.prototype.on = function (eventName, listener) {
+        if (undefined === this.listenersMap[eventName]) {
+            this.listenersMap[eventName] = [];
+        }
+        this.listenersMap[eventName].push(listener);
+        return this;
+    };
+    EventEmitter.prototype.once = function (eventName, listener) {
+        listener.isOnce = true;
+        this.on(eventName, listener);
+        return this;
+    };
+    EventEmitter.prototype.off = function (eventName, listener) {
+        var listeners = this.listenersMap[eventName];
+        if (undefined !== listeners) {
+            if (undefined === listener) {
+                delete this.listenersMap[eventName];
+            }
+            else {
+                var index = listeners.findIndex(function (fn) { return fn === listener; });
+                listeners.splice(index, 1);
+            }
+        }
+        return this;
+    };
+    EventEmitter.prototype.emit = function (eventName) {
+        var payload = [];
+        for (var _i = 1; _i < arguments.length; _i++) {
+            payload[_i - 1] = arguments[_i];
+        }
+        var e_1, _a;
+        var listeners = this.listenersMap[eventName];
+        if (undefined !== listeners && 0 < listeners.length) {
+            try {
+                for (var _b = __values$1(listeners.entries()), _c = _b.next(); !_c.done; _c = _b.next()) {
+                    var _d = __read$1(_c.value, 2), index = _d[0], listener = _d[1];
+                    if (listener.isOnce) {
+                        var listenerClone = listener;
+                        listeners.splice(index, 1);
+                        listenerClone.apply(void 0, __spread$1(payload));
+                    }
+                    else {
+                        listener.apply(void 0, __spread$1(payload));
+                    }
+                }
+            }
+            catch (e_1_1) { e_1 = { error: e_1_1 }; }
+            finally {
+                try {
+                    if (_c && !_c.done && (_a = _b["return"])) _a.call(_b);
+                }
+                finally { if (e_1) throw e_1.error; }
+            }
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    EventEmitter.prototype.destroy = function () {
+        this.listenersMap = {};
+    };
+    return EventEmitter;
+}());
+
 var MOBILE_REGEX = /mobile|tablet|ip(ad|hone|od)|android/i;
 var IS_MOBILE = MOBILE_REGEX.test(navigator.userAgent);
 var SUPPORT_TOUCH = ('ontouchstart' in window);
@@ -85,58 +203,6 @@ var INPUT_START = 'start';
 var INPUT_MOVE = 'move';
 var INPUT_CANCEL = 'cancel';
 var INPUT_END = 'end';
-
-var EventBus = (function () {
-    function EventBus(targetElement) {
-        if (targetElement === void 0) { targetElement = document; }
-        this._stack = {};
-        this.targetElement = targetElement;
-    }
-    EventBus.prototype.el = function (targetElement) {
-        if (targetElement === void 0) { targetElement = document; }
-        this.targetElement = targetElement;
-        return this;
-    };
-    EventBus.prototype.emit = function (eventName, payload) {
-        if (undefined !== this._stack[eventName]) {
-            var callbacks = this._stack[eventName];
-            callbacks.forEach(function (callback) {
-                callback(payload);
-            });
-            var event = new CustomEvent(eventName, {
-                detail: payload,
-                bubbles: true,
-                cancelable: true
-            });
-            this.targetElement.dispatchEvent(event);
-        }
-    };
-    EventBus.prototype.on = function (eventName, callback) {
-        if (undefined === this._stack[eventName]) {
-            this._stack[eventName] = [];
-        }
-        this._stack[eventName].push(callback);
-    };
-    EventBus.prototype.off = function (eventName, callback) {
-        var events = this._stack[eventName];
-        if (undefined === callback) {
-            this._stack[eventName] = [];
-        }
-        else {
-            for (var i = 0, len = events.length; i < len; i++) {
-                var existCallback = events[i];
-                if (existCallback === callback) {
-                    events.splice(i, 1);
-                    break;
-                }
-            }
-        }
-    };
-    EventBus.prototype.has = function (eventName) {
-        return undefined !== this._stack[eventName];
-    };
-    return EventBus;
-}());
 
 var round = Math.round;
 var getVLength = function (v) {
@@ -973,10 +1039,15 @@ var AnyTouch = (function () {
     function AnyTouch(el, options) {
         if (options === void 0) { options = DEFAULT_OPTIONS; }
         var _this = this;
+        var m = new EventEmitter();
+        m.on('add', function (data) {
+            console.log(data);
+        });
+        m.emit('add', 'asdjsa;l123');
         this.version = '0.0.2';
         this.el = el;
         this.isMobile = IS_MOBILE;
-        this.eventBus = new EventBus(el);
+        this.eventBus = new EventEmitter();
         this.options = __assign({}, DEFAULT_OPTIONS, options);
         this.recognizers = [
             new TapRecognizer(),
