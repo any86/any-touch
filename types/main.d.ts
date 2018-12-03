355a9981e42e2570500195e8b9c2dcec7f549f06
宁宁 <383514580@qq.com>
2018-12-03 15:49:21 +0800

    简单写个d.ts测试用

---
+++ types/main.d.ts
@@ -0,0 +1,11 @@
+export { 
+    directionString,
+    EventHandler, 
+    Computed,
+    inputStatus,
+    RecognizerStatus,Input,RecognizerCallbackPaylod,RecognizerCallback,EventBus,Vector
+} from '../src/interface.d';
+declare class AnyTouch {
+    
+}
+export default AnyTouch;
