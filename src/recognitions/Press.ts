4c578d9b4f62b9290e885c349341b90967099ac1
PC-Ning <383514580@qq.com>
2019-01-20 00:45:26 +0800

    修复press在移动大距离后没有失败

--- src/recognitions/Press.ts
+++ src/recognitions/Press.ts
@@ -9,11 +9,9 @@ import { INPUT_CANCEL, INPUT_END, INPUT_MOVE, INPUT_START } from '../const';
 import Recognizer from './Base';
 export default class PressRecognizer extends Recognizer {
     protected _timeoutId: number;
-    private _isPressing: boolean;
     constructor(options: Options = {}) {
         super(options);
         this._timeoutId = null;
-        this._isPressing = false;
     };
 
     getTouchAction() {
@@ -22,45 +20,58 @@ export default class PressRecognizer extends Recognizer {
 
     recognize(computed: Computed): void {
         const { inputStatus } = computed;
-        // 手指一直按住屏幕
+        if (INPUT_START === inputStatus) {
+            this.status = STATUS_POSSIBLE;
+        }
+
+        if(STATUS_FAILED === this.status) return;
+
+
+        // 开始识别
         if (STATUS_RECOGNIZED !== this.status) {
+            // console.log(this.status, computed.distance);
             // 如果未开始按住屏幕 && 限制条件已经通过
             // 那么延迟触发press
-            const IS_VALID = this.test(computed);
-            if (!this._isPressing && IS_VALID) {
-                this._isPressing = true;
-                this._timeoutId = window.setTimeout(() => {
-                    this.status = STATUS_RECOGNIZED;
-                    this.emit(this.options.name, computed);
-                }, this.options.minPressTime);
-            } else {
-                if(!IS_VALID) {
+                const IS_VALID = this.test(computed)
+                // console.log({IS_VALID});
+                if (IS_VALID) {
+                    // 延迟触发
                     this.cancel();
+                    this._timeoutId = window.setTimeout(() => {
+                        this.status = STATUS_RECOGNIZED;
+                        this.emit(this.options.name, computed);
+                    }, this.options.minPressTime);
+                    // console.log('_timeoutId', this._timeoutId);
+                } else {
+                    this.cancel();
+                    this.status = STATUS_FAILED;
                 }
+
+            if(INPUT_END === inputStatus) {
+                this.status = STATUS_FAILED;
+                // console.log(this.status);
             }
         }
         // 已识别 
-        else {
+        else{
             // end阶段触发pressup
             if (INPUT_END === inputStatus) {
                 this.emit(`${this.options.name}up`, computed);
-                // 复位状态
-                this.status = STATUS_POSSIBLE;
-                this._isPressing = false;
             }
         }
     };
 
     public test({ pointerLength, inputStatus, distance }: Computed): boolean {
-        const IS_VALID_INPUT = 'start' === inputStatus || 'move' === inputStatus;
+        // const IS_VALID_INPUT = 'start' === inputStatus || 'move' === inputStatus;
         const IS_VLIAD_DISTANCE = this.options.threshold > distance;
-        return this.isValidPointerLength(pointerLength) && IS_VALID_INPUT && IS_VLIAD_DISTANCE;
+        return this.isValidPointerLength(pointerLength) && IS_VLIAD_DISTANCE;
+
+        // return this.isValidPointerLength(pointerLength) && IS_VALID_INPUT && IS_VLIAD_DISTANCE;
     };
 
-    protected cancel() {
+    public cancel() {
         clearTimeout(this._timeoutId);
-        this.status = STATUS_FAILED;
-        this._isPressing = false;
+        // console.warn('cancel',this._timeoutId);
     }
 
     afterEmit() { }
