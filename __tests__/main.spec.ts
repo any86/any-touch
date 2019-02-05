f69f97f973a2c46efd00327f6884ebfbde3840c8
宁宁 <383514580@qq.com>
2019-02-05 16:45:50 +0800

    chore: 删除无用的注释

--- __tests__/main.spec.ts
+++ __tests__/main.spec.ts
@@ -1,16 +1,11 @@
 // https://jestjs.io/docs/en/manual-mocks
 //必须放在第一行
 // window.ontouchstart = null; //jest.fn();
-// let g :any= global
-// g.ontouchstart = null; //jest.fn();
 import AnyTouch from '../src/main';
 // console.log({ global })
 test('当前是否移动设备?', () => {
-    // console.log('ontouchstart', !!window.ontouchstart);
     // console.log(process.env.NODE_ENV);
     // let { jsdom } = <any>global;
-    // // console.log(jsdom.window);
-    // Object.defineProperty(jsdom.window.navigator, 'userAgent', {value:"Mellblomenator/9000"})
     const el = document.createElement('div');
     
     const at = new AnyTouch(el);
