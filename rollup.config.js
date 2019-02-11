6fafa33e0c013b37de4815f06fbc34c337a3303c
宁宁 <383514580@qq.com>
2019-02-11 11:15:47 +0800

    chore: 用rollup-plugin-json/rollup-plugin-replace实现代码内读取package.json中的version字段

--- rollup.config.js
+++ rollup.config.js
@@ -2,41 +2,57 @@
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript';
-
-
-// const pkg = require('./package.json');
-
+import json from 'rollup-plugin-json';
+import replace from 'rollup-plugin-replace';
+import pkg from './package.json';
+const banner =
+`/*!
+ * AnyTouch.js v${pkg.version}
+ * (c) 2018-${new Date().getFullYear()} Russell
+ * https://github.com/383514580/any-touch
+ * Released under the MIT License.
+ */`
 
 export default {
     input: './src/main.ts',
-    plugins: [typescript({
-        exclude: 'node_modules/**',
-        typescript: require('typescript')
-    }),
+    plugins: [
+        replace({
+            __VERSION__: pkg.version
+        }),
 
-    nodeResolve({
-        jsnext: true,
-        main: true
-    }),
+        typescript({
+            exclude: 'node_modules/**',
+            typescript: require('typescript'),
 
-    commonjs({
-        // non-CommonJS modules will be ignored, but you can also
-        // specifically include/exclude files
-        include: 'node_modules/**',  // Default: undefined
-    })
+        }),
+        json(),
+        nodeResolve({
+            jsnext: true,
+            main: true
+        }),
+
+        commonjs({
+            // non-CommonJS modules will be ignored, but you can also
+            // specifically include/exclude files
+            include: 'node_modules/**', // Default: undefined
+        })
 
     ],
     output: [{
-        format: 'cjs',
-        file: 'dist/AnyTouch.common.js',
-    },
-    {
-        format: 'es',
-        file: 'dist/AnyTouch.es.js',
-    },
-    {
-        format: 'umd',
-        name: 'AnyTouch',
-        file: 'dist/AnyTouch.umd.js',
-    }]
+            format: 'cjs',
+            file: pkg.main,
+            banner
+        },
+        {
+            format: 'es',
+            file: pkg.module,
+            banner
+        },
+        {
+            format: 'umd',
+            name: 'AnyTouch',
+            file: pkg.browser,
+            banner
+        }
+    ]
 };
--- src/main.ts
+++ src/main.ts
@@ -34,29 +34,19 @@ import SwipeRecognizer from './recognitions/Swipe';
 import PinchRecognizer from './recognitions/Pinch';
 import RotateRecognizer from './recognitions/Rotate';
 import * as Vector from './vector';
+const pkg = require('../package.json');
 interface Options {
     touchAction?: 'compute' | 'auto' | 'manipulation' | 'pan-x' | 'pan-y' | 'none';
     hasDomEvents?: boolean;
     isPreventDefault?: boolean;
 };
 export default class AnyTouch {
-    // 识别器
     static TapRecognizer = TapRecognizer;
     static PressRecognizer = PressRecognizer;
     static PanRecognizer = PanRecognizer;
     static SwipeRecognizer = SwipeRecognizer;
     static PinchRecognizer = PinchRecognizer;
     static RotateRecognizer = RotateRecognizer;
-
-    // 缩写
-    static Tap = TapRecognizer;
-    static Press = PressRecognizer;
-    static Pan = PanRecognizer;
-    static Swipe = SwipeRecognizer;
-    static Pinch = PinchRecognizer;
-    static Rotate = RotateRecognizer;
-
-    // 向量计算
     static Vector = Vector;
 
     // 目标元素
@@ -70,9 +60,9 @@ export default class AnyTouch {
 
     unbinders: any[];
 
-    version: string;
-
     options: Options;
+
+    version: string;
 
     eventBus: any;
 
