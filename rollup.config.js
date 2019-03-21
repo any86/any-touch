e4583602a4c2deb654e3893a08fe16930613986f
PC-Ning <383514580@qq.com>
2019-03-21 22:14:48 +0800

    chore(build): 给打包配置文件增加注释

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,19 @@
-// import babel from 'rollup-plugin-babel';
+// 为了让rollup识别commonjs类型的包,默认只支持导入ES6
 import commonjs from 'rollup-plugin-commonjs';
+// 为了支持import xx from 'xxx'
 import nodeResolve from 'rollup-plugin-node-resolve';
+// ts转js的编译器
 import typescript from 'rollup-plugin-typescript';
+// 支持加载json文件
 import json from 'rollup-plugin-json';
+// 支持字符串替换, 比如动态读取package.json的version到代码
 import replace from 'rollup-plugin-replace';
+// 读取package.json
 import pkg from './package.json';
+// 代码生成sourcemaps
 import sourceMaps from 'rollup-plugin-sourcemaps'
 
+// 代码头
 const banner =
     `/*!
  * AnyTouch.js v${pkg.version}
@@ -34,9 +41,7 @@ export default {
         }),
 
         commonjs({
-            // non-CommonJS modules will be ignored, but you can also
-            // specifically include/exclude files
-            include: 'node_modules/**', // Default: undefined
+            include: 'node_modules/**'
         }),
 
         sourceMaps()
@@ -44,12 +49,15 @@ export default {
     ],
     output: [{
             format: 'cjs',
-            file: pkg.main,
+            // 生成的文件名和路径
+            // package.json的main字段, 也就是模块的入口文件
+            file: pkg.main, 
             banner,
             sourcemap: true
         },
         {
             format: 'es',
+            // rollup和webpack识别的入口文件, 如果没有该字段, 那么会去读取main字段
             file: pkg.module,
             banner,
             sourcemap: true
