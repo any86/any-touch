a585ae022deae61eb92abd3d4ce41cef4ed42b45
PC-Ning <383514580@qq.com>
2019-04-22 23:37:55 +0800

    test: jest增加--runInBand, 测试ci下速度问题是否解决

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     preset: 'ts-jest/presets/js-with-babel',
     testEnvironment: 'jsdom',
+    // roots: ['./src/'],
     // testEnvironment: 'jest-environment-jsdom-global',
     testEnvironmentOptions: {
         userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
