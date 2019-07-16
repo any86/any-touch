ec766036dd1767473d8048d8dd073b392aae4160
宁宁 <383514580@qq.com>
2019-07-16 17:28:18 +0800

    refactor: 加入一个浅拷贝函数

--- src/input/adapters/Touch.ts
+++ src/input/adapters/Touch.ts
@@ -1,5 +1,6 @@
 import { BaseInput, eventType } from '../../interface';
 export default class {
+
     load(event: TouchEvent): BaseInput {
         const points = Array.from(event.touches).map(({ clientX, clientY }) => ({ clientX, clientY }));
         const changedPoints = Array.from(event.changedTouches).map(({ clientX, clientY }) => ({ clientX, clientY }));
--- src/utils/copyProps.ts
+++ src/utils/copyProps.ts
@@ -1,11 +1,20 @@
 /**
- * "-"格式转成驼峰格式
- * @param {String} 输入 
- * @return {String} 输出
+ * 拷贝对象上的指定键值, 生成新的对象, 只拷贝第一层
+ * @param {Object} 输入
+ * @param {Array} 要复制的键值 
+ * @return {Object} 输出
  */
-export default (string: string): string=> {
-    var camelizeRE = /-(\w)/g;
-    return string.replace(camelizeRE, word => {
-        return word.toLocaleUpperCase().slice(1)
-    });
+export default <O, K extends keyof O>(object: O, keys: K[]): Record<K, any> => {
+    let _object = <Record<K, any>>{};
+    for (let key of keys) {
+        _object[key] = object[key];
+    }
+    return _object;
 }
+
+type O = { a: number, b: string };
+type K = keyof O;
+
+type A = K[]
+type B = Record<K, any>
+// let arr:A = ['a']
