de7cf91522afdfa0ed8e92760e3be61e3b242958
宁宁 <383514580@qq.com>
2019-09-16 16:18:44 +0800

    fix: 修复MaxLength计算错误

--- src/compute/computeMaxLength.ts
+++ src/compute/computeMaxLength.ts
@@ -2,10 +2,7 @@ import Store from '../Store';
 
 export default ({ pointLength, isStart }: { pointLength: number, isStart: boolean }, $store: Store): number => {
     if (isStart) {
-        const maxLength = $store.get('maxPointLength', 0);
-        if (pointLength > maxLength) {
-            $store.set({ maxPointLength: pointLength });
-        }
+        $store.set({ maxPointLength: pointLength });
         return pointLength;
     }
     return $store.get('maxPointLength', 0)
