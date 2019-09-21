64ea2f4c9109b636b7473a70d2df6933789c109b
宁宁 <383514580@qq.com>
2019-09-22 07:46:40 +0800

    fix: 增加touches作为默认值, 解决wx下没有targetTouches

--- src/input/adapters/Touch.ts
+++ src/input/adapters/Touch.ts
@@ -2,7 +2,8 @@ import { BaseInput, eventType } from '../../interface';
 import Adapter from './Abstract';
 export default class extends Adapter {
     load(event: TouchEvent):BaseInput {
-        const points = Array.from(event.targetTouches).map(({ clientX, clientY }) => ({ clientX, clientY }));
+        // fix: wx下没有targetTouches
+        const points = Array.from(event.targetTouches||event.touches).map(({ clientX, clientY }) => ({ clientX, clientY }));
         const changedPoints = Array.from(event.changedTouches).map(({ clientX, clientY }) => ({ clientX, clientY }));
         return {
             eventType: <eventType>event.type.replace('touch', ''),
