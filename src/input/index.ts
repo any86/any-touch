5cba5c1750e80c4017448b156810871b1bd0a2fc
383514580 <383514580@qq.com>
2019-12-01 21:19:24 +0800

    refactor: Input增加id用来标识每一次的输入

--- src/input/index.ts
+++ src/input/index.ts
@@ -9,12 +9,18 @@ import Adapter from './adapters/Abstract';
 
 export default class {
     public adapter: Adapter;
+    public id: number;
     constructor() {
         this.adapter = SUPPORT_TOUCH ? new Touch() : new Mouse();
+        this.id = 0;
     };
 
     public load(event: SupportEvent): BaseInput | void {
         // 从event中采集的数据
-        return this.adapter.load(event);
+        const input = this.adapter.load(event);
+        if (void 0 !== input) {
+            const id = Number.MAX_SAFE_INTEGER > this.id ? this.id ++ : 1
+            return { ...input, id };
+        }
     };
 }
