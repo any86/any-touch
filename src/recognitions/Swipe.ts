3d0c1b21faf218b541649f03ebecd50773846e64
383514580 <383514580@qq.com>
2019-12-07 00:53:27 +0800

    fix: 修复错误的"间隔时间"造成的错误速度

--- src/recognitions/Swipe.ts
+++ src/recognitions/Swipe.ts
@@ -1,6 +1,12 @@
 import Recognizer from './Base';
-import { AnyTouchEvent } from '@/types';
-import { INPUT_END,DIRECTION_ALL,NONE } from '../const';
+import { InputRecord } from '@/types';
+import { INPUT_END, DIRECTION_ALL, NONE, KEY_DIRECTION, KEY_DISTANCE, KEY_MAX_POINT_LENGTH } from '@/const';
+import computeDistance from '@/compute/computeDistance';
+import computeDeltaXY from '@/compute/computeDeltaXY';
+import intervalCompute from '@/compute/intervalCompute';
+import computeMaxLength from '@/compute/computeMaxLength';
+
+
 export default class SwipeRecognizer extends Recognizer {
     static DEFAULT_OPTIONS = {
         name: 'swipe',
@@ -21,9 +27,9 @@ export default class SwipeRecognizer extends Recognizer {
      * 识别后发布swipeleft等事件
      * @param {AnyTouchEvent} 计算数据
      */
-    afterEmit(computed: AnyTouchEvent) {
-        if (NONE !== computed.direction) {
-            this.emit(this.options.name + computed.direction, computed);
+    afterEmit() {
+        if (NONE !== this.event.direction) {
+            this.emit(this.options.name + this.event.direction, this.event);
         }
     };
 
@@ -31,11 +37,26 @@ export default class SwipeRecognizer extends Recognizer {
      * 识别条件
      * @param {AnyTouchEvent} 计算数据
      */
-    test(computed: AnyTouchEvent): boolean {
-        if (INPUT_END !== computed.eventType) return false;
+    test(inputRecord: InputRecord): boolean {
+        const { input } = inputRecord;
+        const { eventType, pointLength } = input;
 
+        // maxPointLength
+        const maxPointLength = this.event[KEY_MAX_POINT_LENGTH] ? this.event[KEY_MAX_POINT_LENGTH] : computeMaxLength(inputRecord, <any>this.$store);
+
+        // displacementX, displacementY, distanceX, distanceY, distance, overallDirection
+        const { displacementX, displacementY, distanceX, distanceY, distance, overallDirection } = this.event[KEY_DISTANCE] ? this.event[KEY_DISTANCE] : computeDistance(inputRecord, <any>this.$store);
+
+        // velocityX, velocityY, speedX, speedY, direction
+        const { velocityX, velocityY, speedX, speedY, direction } =  intervalCompute(inputRecord, <any>this.$store);
+
+        if (INPUT_END !== eventType) return false;
         // 非end阶段, 开始校验数据
-        const { direction, velocityX, velocityY, maxPointLength, distance } = computed;
+        // const { direction, velocityX, velocityY, maxPointLength, distance } = computed;
+
+
+        this.event = { ...this.event, displacementX, displacementY, distanceX, distanceY, distance, overallDirection, velocityX, velocityY, speedX, speedY, direction, maxPointLength };
+
         // 如果只支持水平或垂直, 那么其他方向速率为0;
         // 有效速率
         let vaildVelocityX: number = velocityX;
@@ -46,7 +67,8 @@ export default class SwipeRecognizer extends Recognizer {
             vaildVelocityX = 0;
         }
 
-        let vaildVelocity = Math.sqrt(vaildVelocityX * vaildVelocityX + vaildVelocityY * vaildVelocityY)
+        const vaildVelocity = Math.sqrt(vaildVelocityX * vaildVelocityX + vaildVelocityY * vaildVelocityY)
+
 
         return this.options.pointLength === maxPointLength &&
             this.options.threshold < distance &&
