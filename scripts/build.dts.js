d0ff946ab7f2c7f66729eeb5ebf432b5dbb12e0e
383514580 <383514580@qq.com>
2020-01-14 22:52:39 +0800

    refactor: 直接通过ts api重写生成类型脚本

--- scripts/build.dts.js
+++ scripts/build.dts.js
@@ -4,7 +4,7 @@ const chalk = require('chalk');
 const ts = require('typescript');
 
 
-function compile(fileNames, options){
+function compile(fileNames, options) {
     // Create a Program with an in-memory emit
     const createdFiles = {}
     const host = ts.createCompilerHost(options);
@@ -13,7 +13,6 @@ function compile(fileNames, options){
     // Prepare and emit the d.ts files
     const program = ts.createProgram(fileNames, options, host);
     program.emit();
-
     // Loop through all the input files
     fileNames.forEach(file => {
         // console.log("### JavaScript\n")
@@ -22,21 +21,42 @@ function compile(fileNames, options){
         // console.log("### Type Definition\n")
         // console.log({file})
 
-        const dts = file.replace(".ts", ".d.ts")
-        // fs.writeFileSync(dts, createdFiles[dts]);
-        // console.log(createdFiles)
-        console.log({dts})
-        // console.log(createdFiles[dts])
+        const dts = file.replace(".ts", ".d.ts");
+        const dist = dts.replace('/src', '/types');
+        const distDir = path.dirname(dist);
+        if (!fs.existsSync(distDir)) {
+            fs.mkdirSync(distDir);
+        }
+
+        fs.writeFileSync(dist, createdFiles[dts]);
+
     })
 }
 
+
+
+const tsFiles = [];
+const PACKAGES_DIR = 'packages';
+const dirs = fs.readdirSync(PACKAGES_DIR);
+for (const dir of dirs) {
+    const stats = fs.statSync(`${PACKAGES_DIR}/${dir}`);
+    if (stats.isDirectory()) {
+        if ('types' !== dir) {
+            const files = fs.readdirSync(`${PACKAGES_DIR}/${dir}/src`);
+            for (const file of files) {
+                const filePath = `${PACKAGES_DIR}/${dir}/src/${file}`;
+                const stats = fs.statSync(filePath);
+                if (stats.isFile()) {
+                    tsFiles.push(filePath)
+                }
+            }
+
+        }
+    }
+}
+
 // Run the compiler
-// compile(['packages/any-event/src/index.ts'], {
-//     allowJs: true,
-//     declaration: true,
-//     emitDeclarationOnly: true,
-// });
-
-const d = path.resolve('packages/any-event/src', '../dist')
-
-console.log({d})
+compile(tsFiles, {
+    declaration: true,
+    emitDeclarationOnly: true,
+});
