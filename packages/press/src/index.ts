c83f4c03cfb724ce6931d54ea1084a6f027b08e0
383514580 <383514580@qq.com>
2020-01-28 21:39:19 +0800

    pref: 跳过start阶段的"距离约束"校验

--- packages/press/src/index.ts
+++ packages/press/src/index.ts
@@ -3,7 +3,7 @@ import {
     STATUS_FAILED, STATUS_RECOGNIZED, DIRECTION_UP, INPUT_CANCEL, INPUT_END, INPUT_START
 } from '@any-touch/shared';
 import { ComputeDistance } from '@any-touch/compute';
-import Recognizer,{ resetStatusForPressMoveLike as resetStatus } from '@any-touch/recognizer';
+import Recognizer, { resetStatusForPressMoveLike as resetStatus } from '@any-touch/recognizer';
 
 export default class extends Recognizer {
     private _timeoutId?: number;
@@ -18,12 +18,12 @@ export default class extends Recognizer {
     };
 
     recognize(input: Input, emit: CommonEmitFunction): void {
-        const { inputType, startInput } = input;
-        const deltaTime = input.timestamp - startInput.timestamp;
+        const { inputType, startInput, pointLength } = input;
+
         // 1. start阶段
         // 2. 触点数符合
         // 那么等待minPressTime时间后触发press
-        if (INPUT_START === inputType && this.test(input)) {
+        if (INPUT_START === inputType && this.isValidPointLength(pointLength)) {
             // 重置状态
             resetStatus(this);
             // 延迟触发
@@ -33,21 +33,23 @@ export default class extends Recognizer {
                 emit(this.options.name, input);
             }, this.options.minPressTime);
         }
-
         // 触发pressup条件:
         // 1. end阶段
         // 2. 已识别
         else if (INPUT_END === inputType && STATUS_RECOGNIZED === this.status) {
             emit(`${this.options.name}${DIRECTION_UP}`, this.computed);
         }
-
-        // 一旦不满足必要条件, 触发失败
-        // 对应cancel和end阶段
-        else if (!this.test(input) || (this.options.minPressTime > deltaTime && -1 !== [INPUT_END, INPUT_CANCEL].indexOf(inputType))) {
-            this.cancel();
-            this.status = STATUS_FAILED;
+        else {
+            const deltaTime = input.timestamp - startInput.timestamp;
+            // 一旦不满足必要条件,
+            // 发生了大的位移变化
+            if (!this.test(input) || 
+            // end 或 cancel触发的时候还不到要求的press触发时间
+            (this.options.minPressTime > deltaTime && [INPUT_END, INPUT_CANCEL].includes(inputType))) {
+                this.cancel();
+                this.status = STATUS_FAILED;
+            }
         }
-
     };
 
     /**
@@ -59,7 +61,7 @@ export default class extends Recognizer {
         type Computed = ReturnType<ComputeDistance['compute']>;
         this.computed = <Computed>this.compute([ComputeDistance], input);
         const { distance } = this.computed;
-        return this.options.positionTolerance > distance && this.isValidPointLength(pointLength);
+        return this.options.positionTolerance > distance;
     };
 
     cancel(): void {
