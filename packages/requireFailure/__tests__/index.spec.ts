6a8ba9938faea885356d7a57d2d3010ad1bc296c
383514580 <383514580@qq.com>
2020-01-30 23:11:46 +0800

    test: 增加测试: 双击失败那么触发单击

--- packages/requireFailure/__tests__/index.spec.ts
+++ packages/requireFailure/__tests__/index.spec.ts
@@ -3,7 +3,7 @@ import AnyTouch from '@any-touch/core';
 import Tap from '@any-touch/tap';
 import requireFailure from '@any-touch/requireFailure';
 import { GestureSimulator, sleep } from '@any-touch/simulator';
-test('单击需要等待300ms, 如果届时双击状态为失败那么触发单击', async (done) => {
+test('识别为双击那么单击不触发', async (done) => {
     const DOUBLE_TAP_NAME = 'doubletap';
     const { mockCB, sleep } = create();
     AnyTouch.use(Tap);
@@ -28,5 +28,31 @@ test('单击需要等待300ms, 如果届时双击状态为失败那么触发单�
     await sleep(350);
     expect(mockCB).toHaveBeenCalledTimes(1);
     expect(mockCB).toHaveBeenCalledWith(DOUBLE_TAP_NAME);
+    done();
+});
+
+
+test('双击失败那么触发单击', async (done) => {
+    const DOUBLE_TAP_NAME = 'doubletap';
+    // AnyTouch.use(Tap);
+    // AnyTouch.use(Tap, { name: DOUBLE_TAP_NAME, tapTimes: 2 });
+    // AnyTouch.use(requireFailure, 'tap', DOUBLE_TAP_NAME);
+    const el = document.createElement('div');
+    const gs = new GestureSimulator(el);
+    const at = new AnyTouch(el);
+    const onTap = jest.fn().mockName('onTap');
+    const onDoubleTap = jest.fn().mockName('onDoubleTap');
+
+    at.on('tap', onTap);
+    at.on(DOUBLE_TAP_NAME, onDoubleTap);
+
+    gs.dispatchTouchStart();
+    gs.dispatchTouchEnd();
+    gs.dispatchTouchStart([{x:100,y:0}]);
+    gs.dispatchTouchEnd();
+
+    await sleep(350);
+    expect(onTap).toHaveBeenCalledTimes(2);
+    expect(onDoubleTap).toHaveBeenCalledTimes(0);
     done();
 });
