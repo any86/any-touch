fe79b2a0e854e13706b99cb4a573badc7f004caa
383514580 <383514580@qq.com>
2020-01-30 14:22:25 +0800

    test: 测试实例上的use/removeUse

--- packages/core/__tests__/index.spec.ts
+++ packages/core/__tests__/index.spec.ts
@@ -75,6 +75,31 @@ test('加载/卸载一个手势,', async done => {
     done();
 });
 
+test(`通过实例加载/卸载插件`, async done=>{
+    const { AnyTouch, el, GestureSimulator, mockCB, sleep } = create();
+    const at = new AnyTouch(el);
+    const gs = new GestureSimulator(el);
+    at.use(Tap);
+    at.on('tap',ev=>{
+        mockCB(ev.type);
+    });  
+
+    gs.dispatchTouchStart();
+    gs.dispatchTouchEnd();
+    await sleep();
+    expect(mockCB).toHaveBeenCalledWith('tap');
+
+    // 卸载
+    at.removeUse('tap');
+    at.on('tap',ev=>{
+        mockCB();
+    }); 
+    await sleep();
+    expect(mockCB).toHaveBeenCalledTimes(1);
+    done();
+});
+
+
 test('destroy实例', () => {
     const { at, touch, sleep, mockCB } = create();
     at.on('at:input', () => {
