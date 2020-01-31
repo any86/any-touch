da168ff9df831606455f13689a5c6105ba9ba027
383514580 <383514580@qq.com>
2020-01-31 23:31:01 +0800

    test: 模拟微信下没有el,通过catchEvent接收数据从而识别手势

---
+++ packages/core/__tests__/noEl.spec.ts
@@ -0,0 +1,19 @@
+import AnyTouch from '../src/index';
+import { GestureSimulator, sleep } from '@any-touch/simulator';
+
+test(`模拟微信下没有el,通过catchEvent接收数据从而识别手势`, async (done) => {
+    const at = new AnyTouch();
+    const el = document.createElement('div');
+    const gs = new GestureSimulator(el);
+    const event = gs.dispatchTouchStart();
+    const onInput = jest.fn();
+    at.on('at:input',onInput);
+    at.catchEvent(event);
+    await sleep();
+    expect(onInput).toHaveBeenCalledTimes(1);
+    // 销毁
+    at.destroy();
+    at.on('at:input',onInput);
+    expect(onInput).toHaveBeenCalledTimes(1);
+    done();
+});
