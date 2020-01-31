5f189de5bb5a981694bc34c847a68ef4d0daf65e
383514580 <383514580@qq.com>
2020-01-31 18:53:30 +0800

    refactor: 不再从targetTouches取值, 因为target方法会过滤target

--- packages/core/src/Input/adapters/Touch.ts
+++ packages/core/src/Input/adapters/Touch.ts
@@ -2,8 +2,8 @@ import { BaseInput, InputType } from '@any-touch/shared';
 import Adapter from './Abstract';
 export default class extends Adapter {
     load(event: TouchEvent): Omit<BaseInput, 'id'> {
-        // fix: wx下没有targetTouches
-        const points = Array.from(event.targetTouches || event.touches).map(({ clientX, clientY }) => ({ clientX, clientY }));
+        // tip: wx下没有targetTouches
+        const points = Array.from(event.touches).map(({ clientX, clientY }) => ({ clientX, clientY }));
         const changedPoints = Array.from(event.changedTouches).map(({ clientX, clientY }) => ({ clientX, clientY }));
         return {
             inputType: <InputType>event.type.replace('touch', ''),
