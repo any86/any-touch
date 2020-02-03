569b61612de20b112742e4bf887838e765a38c68
383514580 <383514580@qq.com>
2020-02-03 10:41:42 +0800

    refactor:过滤不支持的设备

--- packages/core/src/dispatchDomEvent.ts
+++ packages/core/src/dispatchDomEvent.ts
@@ -1,10 +1,12 @@
 /**
  * 触发dom事件
  */
-export default function (el: HTMLElement, payload: Record<string, any>) {
-    // 过滤掉几个Event上保留的字段(target, currentTarget)
+export default function (el: HTMLElement, payload: Record<string, any>, eventInit?: EventInit) {
+    // 过滤不支持的设备
+    if (!Event) return;
+    // 过滤掉Event上保留的字段(target, currentTarget,type)
     let { target, currentTarget, type, ...data } = payload;
-    let event = new Event(type, payload);
+    let event = new Event(type, eventInit);
     Object.assign(event, data);
     el.dispatchEvent(event);
 }
