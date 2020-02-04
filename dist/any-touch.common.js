d08524d7902e903f6981d0a60768b1a11b7409f2
383514580 <383514580@qq.com>
2020-02-04 17:40:02 +0800

    Updates

--- dist/any-touch.common.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./any-touch.common.prod.js');
-} else {
-    module.exports = require('./any-touch.common.dev.js');
-}
--- dist/v-touch.common.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./v-touch.common.prod.js');
-} else {
-    module.exports = require('./v-touch.common.dev.js');
-}
