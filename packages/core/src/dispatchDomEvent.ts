e04633e73823f26c942f1d5ff443286b4867ca6b
宁宁 <383514580@qq.com>
2020-02-11 10:40:37 +0800

    refactor: 用document.createEvent增加IE兼容,

--- packages/core/src/dispatchDomEvent.ts
+++ packages/core/src/dispatchDomEvent.ts
@@ -3,11 +3,15 @@ import { Input } from '@any-touch/shared';
  * 触发dom事件
  */
 export default function (el: EventTarget, payload: Record<string, any> & Input, eventInit?: EventInit): boolean | void {
-    // 过滤不支持的设备
-    if (!Event) return;
     // 过滤掉Event上保留的字段(target, currentTarget,type)
     let { target, currentTarget, type, ...data } = payload;
-    let event = new Event(type, eventInit);
+    let event: Event;
+    if (void 0 !== Event) {
+        event = new Event(type, eventInit);
+    } else {
+        event = document.createEvent('HTMLEvents');
+        event.initEvent(type, eventInit?.bubbles, eventInit?.cancelable);
+    }
     Object.assign(event, data, {
         exact: () =>
             payload.targets.every(target =>
