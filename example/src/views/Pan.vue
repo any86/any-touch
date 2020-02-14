5401a983a249f5f165206f7796512ecaf713e063
383514580 <383514580@qq.com>
2020-02-14 23:50:52 +0800

    chore: 删除无用

---
+++ example/src/views/Pan.vue
@@ -0,0 +1,43 @@
+<template>
+    <main>
+        <section ref="child">Child</section>
+    </main>
+</template>
+
+<script>
+import AnyTouch from '@any-touch/core';
+import Pan from '@any-touch/pan';
+AnyTouch.use(Pan);
+export default {
+    name: 'Map',
+
+    data() {
+        return {
+          
+        };
+    },
+
+    mounted(){
+        const at = new AnyTouch(this.$refs.child);
+        at.on('pan', ev=>{
+            console.log(ev)
+        })
+        
+    },
+
+    methods: {
+        onPan(ev) {
+           
+        }
+    }
+};
+</script>
+
+<style scope lang="scss">
+section{
+    padding:24px;
+    width:50%;
+    background:#68c;
+    color:#fff;
+}
+</style>
