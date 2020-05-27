6c2e92882fa973effda2dafe8e70d1e094b8b026
宁宁 <383514580@qq.com>
2020-05-27 17:28:04 +0800

    wip

--- example/src/views/Pan.vue
+++ example/src/views/Pan.vue
@@ -1,40 +1,96 @@
 <template>
-    <main>
-        <section ref="child">Child</section>
-    </main>
+    <ul class="scroll-view">
+        <li
+            class="scroll-view__item"
+            :class="{'scroll-view__item--drag':activeIndex===index}"
+            v-for="(n,index) in 10"
+            :key="n"
+            @press="onPress(index)"
+            @pressup="onPressup"
+        >
+            <div class="text-block">Slide {{n}}</div>
+        </li>
+    </ul>
 </template>
 
 <script>
-import AnyTouch from '@any-touch/core';
-import Pan from '@any-touch/pan';
-AnyTouch.use(Pan);
+import Swiper from 'swiper';
+import 'swiper/css/swiper.css';
+import { Sortable } from 'sortablejs';
+import AnyTouch from '../../../packages/any-touch/dist/any-touch.umd';
 export default {
     name: 'Map',
+
     data() {
-        return {
-          
-        };
+        return { activeIndex: -1, sortable: null };
     },
-    mounted(){
-        const at = new AnyTouch(this.$refs.child);
-        at.on('pandown', ev=>{
-            console.log(`pandown`)
-        })
-        
+
+    mounted() {
+        const at = new AnyTouch(this.$el, { isPreventDefault: false });
+        this.sortable = new Sortable(this.$el, {
+            delay: 200,
+            touchStartThreshold: 3,
+            onChoose(e) {
+                console.log(e);
+            },
+            animation: 300,
+            disabled: !true,
+            scroll: true, // Enable the plugin. Can be HTMLElement.
+            scrollSensitivity: 50, // px, how near the mouse must be to an edge to start scrolling.
+            scrollSpeed: 10, // px, speed of the scrolling
+            bubbleScroll: true // apply autoscroll to all parent elements, allowing for easier movement
+        });
     },
     methods: {
-        onPan(ev) {
-           
+        onPress(index) {
+            this.activeIndex = index;
+            // this.sortable.option('disabled', true);
+        },
+
+        onPressup() {
+            this.activeIndex = -1;
+            // this.sortable.option('disabled', false);
         }
     }
 };
 </script>
 
 <style scope lang="scss">
-section{
-    padding:24px;
-    width:50%;
-    background:#68c;
-    color:#fff;
+.sortable-ghost {
+    opacity: 0.6;
+    border: 1px dashed red;
+    box-sizing: border-box;
+}
+.sortable-drag {
+    box-shadow: 1px 2px 8px rgba(0, 0, 0, 0.2);
+}
+
+.text-block {
+    background-color: #ccc;
+    width: 100px;
+    text-align: center;
+    height: 100px;
+    line-height: 100px;
+
+
+}
+
+.scroll-view {
+    width: 100%;
+    overflow-x: auto;
+    display: flex;
+    -webkit-overflow-scrolling: touch;
+    user-select: none;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    &__item {
+        flex-shrink: 0;
+        flex-grow: 0;
+        list-style: none;
+        &--drag{
+            border: 1px dashed red;
+        }
+    }
 }
 </style>
--- example/src/views/SortCarousel.vue
+++ example/src/views/SortCarousel.vue
@@ -1,24 +1,36 @@
 <template>
     <section class="carousel" :style="{height:`${height}px`}">
-        <ul class="carousel__body" :style="{transform:`translateX(${translateX}px)`}">
+        <ul
+            ref="body"
+            class="carousel__body"
+            @panmove="onPanmove"
+            :style="{transform:`translateX(${translateX}px)`}"
+        >
             <li
-                :draggable="'sort' === action"
+                ref="item"
                 class="carousel__body__item"
+                :class="{'carousel__body__item--cloned':isClonedItem(index)}"
+                :style="[isClonedItem(index)?{
+                    transform: `translateX(${pressItemX + pressMoveX}px)`
+                }:void 0]"
                 :action="index == activeIndex? action: void 0"
-                v-for="(n,index) in 10"
-                :key="n"
+                v-for="({name},index) in list"
+                :key="name+index"
                 @press="onPress(index, $event)"
                 @pressup="onPressup"
-                @panmove="onPanmove(index,$event)"
             >
-                <div class="test-block">{{index}} - {{action}}</div>
+                <div class="test-block">{{name}}</div>
             </li>
         </ul>
+        <h1
+            style="position:absolute;top:0;left:0;z-index:2;color:#fff;"
+        >@{{action}} - {{pressMoveX}}</h1>
     </section>
 </template>
 
 <script>
 import AnyTouch from '../../../packages/any-touch/dist/any-touch.umd';
+import cloneDeep from 'lodash/cloneDeep';
 export default {
     name: 'SortCarousel',
 
@@ -31,10 +43,24 @@ export default {
 
     data() {
         return {
+            list: [{ name: '苹果' }, { name: '笔记本' }, { name: '蔬菜' }, { name: '高楼大厦' }, { name: '铁皮' }],
             translateX: 0,
             action: '',
-            activeIndex: -1
+            activeIndex: -1,
+            elCloned: null,
+            pressMoveX: 0,
+            pressItemX: 0
         };
+    },
+
+    computed: {
+        offsetLefts() {
+            return this.$refs.item.map((el) => el.offsetLeft);
+        },
+        clonedLeft() {
+            return this.offsetLefts[this.activeIndex];
+            // :style="{left:offsetLefts[activeIndex]+'px'}"
+        }
     },
 
     mounted() {
@@ -44,29 +70,59 @@ export default {
         });
     },
     methods: {
-        onPanmove(index, e) {
+        isClonedItem(index) {
+            return 'sort' === this.action && index == this.list.length - 1;
+        },
+
+        onPanmove(e) {
+            console.warn('panmove')
+            const { left, top } = this.$refs.body.getBoundingClientRect();
+            const x = e.x - left;
+            const y = e.y - top;
+
+            const onItemIndex = this.offsetLefts.findIndex((n) => n > x) - 1;
+            if (onItemIndex !== this.activeIndex) {
+                const a = cloneDeep(this.list[this.activeIndex]);
+                const b = cloneDeep(this.list[onItemIndex]);
+                console.warn(a,b)
+                this.list.splice(this.activeIndex, 1, b);
+                this.list.splice(onItemIndex, 1, a);
+                // this.activeIndex = onItemIndex;
+            }
+            console.log(onItemIndex, this.activeIndex);
             if ('' === this.action) {
                 this.translateX += e.deltaX;
             } else if ('sort' === this.action) {
-
+                this.pressMoveX += e.deltaX;
             }
         },
 
-        onPress(index,e) {
+        onPress(index, e) {
+            this.cloneItem(index);
             this.action = 'sort';
             this.activeIndex = index;
+            this.pressMoveX = 0;
+            this.pressItemX = this.offsetLefts[index];
             console.warn('press');
         },
 
         onPressup() {
             this.action = '';
             this.activeIndex = -1;
+            this.removeItemCloned();
             console.warn('pressup');
         },
 
-        cloneItem(){
-            
-        }
+        cloneItem(index) {
+            const active = this.list[index];
+            this.list.push(active);
+        },
+
+        removeItemCloned() {
+            this.list.pop();
+        },
+
+        onTouchmove(index, e) {}
     }
 };
 </script>
@@ -85,22 +141,32 @@ export default {
             }
 
             &[action='sort'] {
-                transition: all 200ms;
                 opacity: 0.3;
                 transform: scale(0.96);
+            }
+
+            &--cloned {
+                // pointer-events: none;
+                position: absolute;
+                opacity: 0.7;
+                top: 0;
+                border-radius: 9999px;
+                .test-block {
+                    background-color: rgba(0, 0, 0, 0.9);
+                }
             }
         }
     }
 }
 
 .test-block {
-    width: 200px;
+    width: 150px;
     height: 100px;
     line-height: 100px;
-    background-color: #555;
     color: #fff;
     font-size: 25px;
     text-align: center;
     margin-right: 16px;
+    background-color: rgb(186, 72, 72);
 }
 </style>
