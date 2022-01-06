4bc5ef618101780f21aa00b2afc9eb37fe66d20c
Russell <383514580@qq.com>
2022-01-06 14:28:28 +0800

    chore: 更新umd中terser的配置

--- scripts/build.umd.js
+++ scripts/build.umd.js
@@ -6,9 +6,9 @@ const {
 const {
     build
 } = require('./build');
-const {compress} = require('minimist')(process.argv.slice(2));
+const { compress } = require('minimist')(process.argv.slice(2));
 
-console.log(chalk.blue(`🤖 正在${compress?'压缩':'生成'}umd模块!`));
+console.log(chalk.blue(`🤖 正在${compress ? '压缩' : '生成'}umd模块!`));
 
 const options = {
     input: `./packages/any-touch/src/index.ts`,
@@ -16,19 +16,19 @@ const options = {
         file: `./packages/any-touch/dist/any-touch.umd.js`,
         format: 'umd',
         name: 'AnyTouch',
-        sourcemap:true
+        sourcemap: true
     },
     tsConfig: {
         target: "ES5",
     },
-    terser: terser({
-        // include: [/^.+\.min\.js$/],
+
+}
+if (compress) {
+    options.output.file = options.output.file.replace('.js', '.min.js');
+    options.terser = terser({
         output: {
             comments: false
         }
     })
-}
-if(compress){
-    options.output.file =options.output.file.replace('.js', '.min.js');
 }
 build(options);
