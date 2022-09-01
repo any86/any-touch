99ef5691395e1bba26ecbe1b4d28f11e9fdcfe75
Russell <383514580@qq.com>
2022-09-01 10:10:16 +0800

    chore: 取消demo的publicPath配置

--- example/vue.config.js
+++ example/vue.config.js
@@ -1,15 +1,15 @@
 // const path = require('path');
 module.exports = {
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ?
-    './' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ?
+  //   './' : '/',
   configureWebpack: {
 
     resolve: {
 
       alias: {
         '@any-touch': '../../../packages',
-        'any-event':'../../../packages/any-event/dist/index.js'
+        'any-event': '../../../packages/any-event/dist/index.js'
       }
     }
   }
